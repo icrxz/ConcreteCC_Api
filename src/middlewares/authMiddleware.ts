681fb5b09d89b8b5908f3a209c917119aa4e34a3
Gteixeirinha7 <gabrielteixeir137@gmail.com>
2020-09-23 20:15:15 -0300

    Remove log

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,13 +3,9 @@ import * as jwt from '../utils/jwt'
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers["auth"] as string;
-  console.log(req.headers);
-  console.log(token);
-
+  
   try {
     const payload = jwt.verify(token) as any;
-    console.log(payload);
-
     res.locals.jwtPayload = payload;
 
     const { userId, email } = payload;
