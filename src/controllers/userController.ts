0ee8fc3bc1a7b1a392b3d44cd5fadb8fc4c7a475
Igor Cruz <igor_cruz_avelino@hotmail.com>
2020-09-23 23:59:42 -0300

    Config debugger

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,13 +2,7 @@ import { Request, Response } from 'express';
 import Bcrypt from 'bcrypt';
 
 import { UserModel }  from '../models/users/users.schema'
-import { S3 } from 'aws-sdk';
 import * as jwt from '../utils/jwt';
-
-
-const s3 = new S3({
-
-})
 
 export const createUser = async (req: Request, res: Response) => {
   try {
@@ -23,7 +17,7 @@ export const createUser = async (req: Request, res: Response) => {
   }
 };
 
-export const indexUser = async (req: Request, res: Response) => {
+export const indexUser = async (res: Response) => {
   try {
     const users = await UserModel.find();
 
@@ -33,7 +27,7 @@ export const indexUser = async (req: Request, res: Response) => {
   }
 };
 
-export const showUser = async (req: Request, res: Response, userId: string) => {
+export const showUser = async (res: Response, userId: string) => {
   try {
     const user = await UserModel.findById(userId).orFail(Error);
 
@@ -43,9 +37,8 @@ export const showUser = async (req: Request, res: Response, userId: string) => {
   }
 };
 
-export const changePassword = async (req: Request, res: Response, userId: string) => {
+export const changePassword = async (req: Request, res: Response) => {
   try {
-
     const token = req.headers['auth'] as string;
     const { userId } = jwt.verify(token) as any;
 
