e33e201646881504b117ed25756debf87a317617
Gteixeirinha7 <gabrielteixeir137@gmail.com>
2020-09-23 22:50:19 -0300

    Remove User delete

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,8 +3,8 @@ import Bcrypt from 'bcrypt';
 
 import { UserModel }  from '../models/users/users.schema'
 import { S3 } from 'aws-sdk';
+import * as jwt from '../utils/jwt';
 
-import * as utils from '../utils/utils';
 
 const s3 = new S3({
 
@@ -12,7 +12,7 @@ const s3 = new S3({
 
 export const createUser = async (req: Request, res: Response) => {
   try {
-    utils.fillCreatedBy(req);
+
     req.body.password = Bcrypt.hashSync(req.body.password, 10);
 
     const user = await UserModel.create(req.body);
@@ -45,8 +45,9 @@ export const showUser = async (req: Request, res: Response, userId: string) => {
 
 export const changePassword = async (req: Request, res: Response, userId: string) => {
   try {
-    
-    utils.fillLastModifiedBy(req);
+
+    const token = req.headers['auth'] as string;
+    const { userId } = jwt.verify(token) as any;
 
     const { lastPassword, newPassword, confirmationPassword } = req.body;
     const user = await UserModel.findById(userId).orFail(Error);
@@ -57,24 +58,9 @@ export const changePassword = async (req: Request, res: Response, userId: string
       ) {
 
         user.password = Bcrypt.hashSync(newPassword, 10);
+        user.lastModifiedById = userId;
         user.save();
     }
-
-    return res.json(user)
-  } catch (error) {
-    return res.status(404).json({ error: 'User not found' })
-  }
-};
-
-export const deleteUser = async (req: Request, res: Response, userId: string) => {
-  try {
-
-    utils.fillLastModifiedBy(req);
-
-    const user = await UserModel.findById(userId).orFail(Error);
-
-    user.isActive = false;
-    user.save();
 
     return res.json(user)
   } catch (error) {
